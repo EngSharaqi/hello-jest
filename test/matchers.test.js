891e2f7b1e955453ffe8fa19b563237253218602
EngSharaqi <muhammadsharaqi@gmail.com>
2022-05-13 18:07:06 +0200

    mathcers

--- test/matchers.test.js
+++ test/matchers.test.js
@@ -45,4 +45,47 @@ describe('Matchers', () => {
             expect(isNaN(myArr[8])).not.toBeTruthy();
         });
     });
+
+    describe('greater than, less than, greater than or equal to, less than or equal to', () => {
+        it('toBeGreaterThan: if its > 1, it will be true', () => {
+            expect(myArr[5]).toBeGreaterThan(1);
+        });
+
+        it('toBeLessThan: if its < 10, it will be true', () => {
+            expect(myArr[5]).toBeLessThan(10);
+        });
+
+        it('toBeGreaterThanOrEqual: if its >= 6, it will be true', () => {
+            expect(myArr[5]).toBeGreaterThanOrEqual(6);
+        });
+
+        it('toBeLessThanOrEqual: if its <= 6, it will be true', () => {
+            expect(myArr[5]).toBeGreaterThanOrEqual(6);
+        });
+    });
+
+    describe('toBeUndefined: if its undefined it will work', () => {
+        it('undefined', () => {
+            let a;
+            expect(a).toBeUndefined();
+        });
+    });
+
+    describe('Object methodes: toHaveProperty :', () => {
+        it('you can check with property only or property and value', () => {
+            let a = {
+                name: 'Mohamed Sharaqi',
+                Age: 24
+            };
+            expect(a).toHaveProperty('name');
+            expect(a).toHaveProperty('name', 'Mohamed Sharaqi');
+        });
+    });
+
+    describe('Object toMatch', () => {
+        it('you can check with property only or property and value', () => {
+            let a = 'Mohamed Sharaqi';
+            expect(a).toMatch(/Sharaqi/);
+        });
+    });
 });
